cc30f6499f9e7e84c13bbca4f775216a34f3facc
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-04-13 16:33:32 +0200

    Add ViewModel For Create Game

---
+++ GameZone/ViewModels/CreateGameFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GameZone.ViewModels
+{
+    public class CreateGameFormViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        public List<int> SelectedDevies { get; set; } = new List<int>();
+
+        public IEnumerable<SelectListItem> Devies { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+
+        public IFormFile Cover { get; set; } = default!;
+
+    }
+}
