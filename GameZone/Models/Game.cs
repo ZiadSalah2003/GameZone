88f9c7b814a1cff9eadbb26a0756e30ab2df8a40
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-04-17 17:11:52 +0200

    update Device name

--- GameZone/Models/Game.cs
+++ GameZone/Models/Game.cs
@@ -11,7 +11,7 @@ namespace GameZone.Models
         public string Cover { get; set; } = string.Empty;
         public Category Category { get; set; } = default!;
         public int CategoryId { get; set; }
-        public ICollection<GameDevice> Device { get; set; } = new List<GameDevice>();
+        public ICollection<GameDevice> Devices { get; set; } = new List<GameDevice>();
 
     }
 }
