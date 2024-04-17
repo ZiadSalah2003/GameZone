c8987fdc9d4487d4894c5761737dcdde39035f00
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-04-17 17:12:12 +0200

    add Service for games

---
+++ GameZone/Services/GamesServices.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace GameZone.Services
+{
+	public class GamesServices
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly IWebHostEnvironment _webHostEnvironment;
+		private readonly string _imagesPath;
+		public GamesServices(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+		{
+			_context = context;
+			this._webHostEnvironment = webHostEnvironment;
+			_imagesPath = $"{_webHostEnvironment.WebRootPath}{FileSettings.ImagePath}";
+		}
+		public async Task Create(CreateGameFormViewModel model)
+		{
+			var coverName = $"{Guid.NewGuid()}{Path.GetExtension(model.Cover.FileName)}";
+			var path = Path.Combine(_imagesPath,coverName);
+			using var stream = File.Create(path);
+			await model.Cover.CopyToAsync(stream);
+			Game game = new()
+			{
+				Name = model.Name,
+				Description = model.Description,
+				CategoryId = model.CategoryId,
+				Cover = coverName,
+				Devices = model.SelectedDevies.Select(d => new GameDevice { DeviceId = d }).ToList()
+			};
+			_context.Add(game);
+			_context.SaveChanges();
+		}
+	}
+}
