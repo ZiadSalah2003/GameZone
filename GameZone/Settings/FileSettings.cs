63581c510a4255fdb8f4c510697565a87338a31f
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-04-17 17:13:25 +0200

    Add Setting for validation and imagepath

---
+++ GameZone/Settings/FileSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace GameZone.Settings
+{
+	public static class FileSettings
+	{
+		public const string ImagePath = "/assets/images/games";
+		public const string AllowedExtenstion = ".jpg,.jpeg,.png";
+		public const int MaxFileSizeInMB = 1;
+		public const int MaxFileSizeInByte = MaxFileSizeInMB * 1024 * 1024;
+	}
+}
