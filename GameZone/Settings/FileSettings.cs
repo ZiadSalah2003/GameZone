aec6e8d952f3e103589bdbfcb1b6cbf3b11ad226
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-05-02 13:24:02 +0300

    Update FileSetting

--- GameZone/Settings/FileSettings.cs
+++ GameZone/Settings/FileSettings.cs
@@ -3,7 +3,7 @@
 	public static class FileSettings
 	{
 		public const string ImagePath = "/assets/images/games";
-		public const string AllowedExtenstion = ".jpg,.jpeg,.png";
+		public const string AllowedExtenstion = ".jpg, .jpeg, .png";
 		public const int MaxFileSizeInMB = 1;
 		public const int MaxFileSizeInByte = MaxFileSizeInMB * 1024 * 1024;
 	}
